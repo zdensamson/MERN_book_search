7ae688caafaa083a48ce70e9155d39efece1c25d
Deku <zachdsamson@gmail.com>
2021-11-07 14:40:19 -0700

    more comments for clarity

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,12 +1,12 @@
 import { gql } from '@apollo/client';
 
+// a query to grab the current user who if they have a new saved or removed book-- this will be updated immediately to generate on the SAVED BOOKS PAGE
 export const GET_ME = gql`
   {
     me {
       _id
       username
       email
-      bookCount
       savedBooks {
         bookId
         authors
